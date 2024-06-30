5519fca2cd7570500fb8e832341386cb9f0896e5
juliaret <julia.retuertanogales@alumnostrazos.net>
2024-06-30 23:50:39 +0200

    practica-casa-30-junio

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,17 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
+
+const gameChoices = document.getElementById('game-choices');
+
+const chooseOption = event => {
+    const choice = event.target.dataset.choice
+  if (event.target.dataset.choice === 'paper') {
+    console.log(`Has escogido papel`);
+  } else if (event.target.dataset.choice === 'scissors') {
+    console.log(`Has escogido tijeras`);
+  } /* else {
+    console.log(`Has escogido piedra`);
+  } */
+};
+
+gameChoices.addEventListener('click', chooseOption);
